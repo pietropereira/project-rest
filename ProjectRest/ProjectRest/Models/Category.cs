353de0055184ac9641456199814af8779d7aadb8
Pietro Pereira <piietro.pereira@hotmail.com>
2021-11-21 22:13:18 -0300

    Models and packages

---
+++ ProjectRest/ProjectRest/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectRest.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public string Name { get; set; }
+    }
+}
---
+++ ProjectRest/ProjectRest/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectRest.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo é preço obrigatório")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O Id da Categoria é obrigatório")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
