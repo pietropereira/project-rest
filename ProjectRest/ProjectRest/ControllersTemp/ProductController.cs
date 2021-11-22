63603f2d468eb2c8d81d3ea07b1233e2f20d1c76
Pietro Pereira <piietro.pereira@hotmail.com>
2021-11-21 22:55:56 -0300

    Config startup / DataContext / FirstMigration / ControllersTemp(no azure)

---
+++ ProjectRest/ProjectRest/ControllersTemp/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectRest.Data;
+using ProjectRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectRest.ControllersTemp
+{
+    [ApiController]
+    [Route("rest/products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> GetAll([FromServices] DataContext context)
+        {
+            var product = await context.Products.Include(x => x.Category).ToListAsync();
+            return product;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Product>> Post([FromServices] DataContext context, [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+    }
+}
---
+++ ProjectRest/ProjectRest/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectRest.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=WHOAMI\\wHOAMI;Initial Catalog=restapi;Data Source=WHOAMI\\SQLEXPRESS");
+        }
+    }
+}
